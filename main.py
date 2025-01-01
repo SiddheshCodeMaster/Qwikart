c77f038ecd18427644bb6f65b1ff3a90b09ca008
Siddhesh Shankar <siddhesh.codemaster.github@gmail.com>
2025-01-01 20:19:58 +0530

    Initial Data Commit for Products

--- main.py
+++ main.py
@@ -5,3 +5,17 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"message":"Hello User, welcome to Qwikart..."} 
+
+@app.get("/get_all_products")
+def get_products():
+    return {"product_data": {"product":[{"Tropicana Natural Fresh Orange Juice":
+                                        {"Price":80, 
+                                         "Description":"Tropicana Natural Fresh Orange Juice, 100 percent Fresh and made up of real oranges",
+                                         "Quantity": 100}},
+                                         {"Prime USA Energy Drink":
+                                            {"Price":330, 
+                                            "Description":"Prime USA Energy Drink: KSI Flavour",
+                                            "Quantity": 50}}
+                                        ]
+                            }
+            }
