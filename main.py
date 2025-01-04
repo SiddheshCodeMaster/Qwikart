ad495816432892e0821b8410912ccad0d44a16a7
SiddheshCodeMaster <siddhesh.shankar.codemaster@gmail.com>
2025-01-04 12:56:13 +0530

    Data Parsing completed for Newly added products

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import Body, FastAPI
 
 app = FastAPI()
 
@@ -8,14 +8,28 @@ def root():
 
 @app.get("/get_all_products")
 def get_products():
-    return {"product_data": {"product":[{"Tropicana Natural Fresh Orange Juice":
-                                        {"Price":80, 
-                                         "Description":"Tropicana Natural Fresh Orange Juice, 100 percent Fresh and made up of real oranges",
-                                         "Quantity": 100}},
-                                         {"Prime USA Energy Drink":
-                                            {"Price":330, 
-                                            "Description":"Prime USA Energy Drink: KSI Flavour",
-                                            "Quantity": 50}}
-                                        ]
-                            }
-            }
+    return {
+  "product_data": {
+    "product": [
+      {
+        "Tropicana Natural Fresh Orange Juice": {
+          "Price": 80,
+          "Description": "Tropicana Natural Fresh Orange Juice, 100 percent Fresh and made up of real oranges",
+          "Quantity": 100
+        }
+      },
+      {
+        "Prime USA Energy Drink": {
+          "Price": 330,
+          "Description": "Prime USA Energy Drink: KSI Flavour",
+          "Quantity": 50
+        }
+      }
+    ]
+  }
+}
+
+@app.post("/createProducts")
+def create_products(payloadProduct : dict = Body(...)):
+    print(payloadProduct["product_data"])
+    return {"added_products":payloadProduct}
