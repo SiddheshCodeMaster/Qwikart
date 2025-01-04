b0a7ef10a07bbe0f2fe6c23107b1e5220d4e832c
SiddheshCodeMaster <siddhesh.shankar.codemaster@gmail.com>
2025-01-04 18:26:57 +0530

    Data Parsing completed for Newly added products with REQUEST ~ RESPONSE changed

--- main.py
+++ main.py
@@ -9,27 +9,30 @@ def root():
 @app.get("/get_all_products")
 def get_products():
     return {
-  "product_data": {
-    "product": [
-      {
-        "Tropicana Natural Fresh Orange Juice": {
-          "Price": 80,
-          "Description": "Tropicana Natural Fresh Orange Juice, 100 percent Fresh and made up of real oranges",
-          "Quantity": 100
+        "product_data": {
+            "Tropicana Natural Fresh Orange Juice": {
+                "Price": 80,
+                "Description": "Tropicana Natural Fresh Orange Juice, 100 percent Fresh and made up of real oranges",
+                "Quantity": 100
+            },
+            "Prime USA Energy Drink": {
+                "Price": 330,
+                "Description": "Prime USA Energy Drink: KSI Flavour",
+                "Quantity": 50
+            }
         }
-      },
-      {
-        "Prime USA Energy Drink": {
-          "Price": 330,
-          "Description": "Prime USA Energy Drink: KSI Flavour",
-          "Quantity": 50
-        }
-      }
-    ]
-  }
-}
+    }
 
 @app.post("/createProducts")
 def create_products(payloadProduct : dict = Body(...)):
-    print(payloadProduct["product_data"])
-    return {"added_products":payloadProduct}
+    
+    # Process the incoming data to store in the required structure
+    added_products = []
+    for product_name, information in payloadProduct.items():
+        added_products.append({
+            "product_name": product_name,
+            "information": information
+        })
+
+    return {"added_products": added_products}
+
