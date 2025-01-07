cc72065eaff7953a94fb7a3dd47fb44ce9e3587c
SiddheshCodeMaster <siddhesh.shankar.codemaster@gmail.com>
2025-01-07 11:12:16 +0530

    Dataset patch

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 from fastapi import Body, FastAPI
 
+from pydantic import BaseModel
+
 # Reading the welcome page:
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 import os
+import json
 
 app = FastAPI()
+
+# Pydantic BaseModel for product information
+class ProductInfo(BaseModel):
+    Price: float
+    Description: str
+    Quantity: int
+
+# Pydantic BaseModel for new product data
+class Product(BaseModel):
+    product_name: str
+    information: ProductInfo
 
 # Mount the static folder for serving static files
 app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -21,23 +35,21 @@ def root():
     
     return HTMLResponse(content=html_content)
 
+# Endpoint to retrieve all products
 @app.get("/get_all_products")
 def get_products():
+    
+    # Load the product data from items.json file
+    with open('dataset/items.json', 'r') as file:
+        products = json.load(file)
+    
+    # Return the data in the desired format
     return {
-        "product_data": {
-            "Tropicana Natural Fresh Orange Juice": {
-                "Price": 80,
-                "Description": "Tropicana Natural Fresh Orange Juice, 100 percent Fresh and made up of real oranges",
-                "Quantity": 100
-            },
-            "Prime USA Energy Drink": {
-                "Price": 330,
-                "Description": "Prime USA Energy Drink: KSI Flavour",
-                "Quantity": 50
-            }
-        }
-    }
+        "product_data": products
+	}
 
+
+# Endpoint to create new products
 @app.post("/createProducts")
 def create_products(payloadProduct : dict = Body(...)):
     
